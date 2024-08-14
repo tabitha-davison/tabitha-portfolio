3fdc7730547cdb5a8b57fe82c15c2e9473bbf2d5
Rowan Jansens <rowan.jansens@gmail.com>
2024-08-13 23:29:46 -0400

    new resume

---
+++ projects/Regolith/animation.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+  setTimeout(function() {
+    console.log('Page is loaded');
+    document.getElementById('view1').classList.add('fade-in');
+    document.getElementById('view2').classList.add('fade-in');
+
+    
+  }, 100);
+};
+
+
+
+ 
+
